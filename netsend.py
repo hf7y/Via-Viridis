af8c641c1a0a535ed097595f496b2aae9aa4837d
zach <dangerpine@gmail.com>
2023-11-22 15:02:39 -0600

    multiple addresses in netsend, add netreceive.pd

--- netsend.py
+++ netsend.py
@@ -6,17 +6,23 @@ import subprocess
 
 GPIO.setmode(GPIO.BOARD)
 
+#identify this device among other senders on the network
+THIS_BOARD = 0
+
+TARGET_ADDRESSES = ["192.168.0.7"]
+
 #set the GPIO input pins
 pirPin = 8
 
 GPIO.setup(pirPin, GPIO.IN)
 
-subprocess.call("pd-extended yourpatch.pd &", shell=True)
+#subprocess.call("pd-extended yourpatch.pd &", shell=True)
 
 pirValue = 0
 
 def send2Pd(message=''):
-    os.system("echo '" + message + "' | pdsend 3000 localhost udp")
+	for addr in TARGET_ADDRESSES:
+	    os.system("echo '" + str(THIS_BOARD) + " " + message + "' | pdsend 3000 " + addr + " udp")
 
 while True:
     pirValue = GPIO.input(pirPin)
